5f6be2d255ea1adb95c2066bfa77c13a9ca850f0
Noah-Huff <>
2021-10-30 18:00:52 -0500

    basic db connection

--- backend/db/connect.js
+++ backend/db/connect.js
@@ -9,5 +9,34 @@ const dbCredentials = {
     port: process.env.DBPORT,
 };
 
+const pool = new Pool(dbCredentials);
+
+pool.connect();
+
+pool.query('SELECT NOW()', (err, res) => {
+    //console.log(err, res);
+    pool.end();
+});
+
+const getAllWords = () => {
+    return new Promise((resolve, reject) => {
+        const text = 'SELECT * FROM testwords';
+        resolve(pool.query(text));
+    });
+}
+
+async function getAsyncData() {
+    //let data =  await getAllWords();
+    return await getAllWords();
+}
+
+const logDetails = async (word) => {
+    let data = await word;
+    console.log('Logging:', data.rows);
+}
+
+logDetails(getAsyncData());
+
+
 module.exports =  {
 };
---
+++ frontend/src/components/boringButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const boringButton = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default boringButton
